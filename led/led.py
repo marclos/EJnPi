5d3ff7164a3510acc2ea17d252f3271a4c83d35e
Kyle McCarty <kyle.mccarty@pomona.edu>
2020-09-30 09:31:11 -0700

    Updated led.py

--- led/led.py
+++ led/led.py
@@ -1,10 +1,16 @@
 import RPi.GPIO as GPIO
-import time
+from time import sleep
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
-print "LED on"
-GPIO.output(18,GPIO.HIGH)
-time.sleep(1)
-print "LED off"
-GPIO.output(18,GPIO.LOW)
+GPIO.setup(18,GPIO.OUT, initial=GPIO.LOW)
+
+print("Press Ctrl + C to exit")
+
+while True:
+  print("LED on"}
+  GPIO.output(24,GPIO.HIGH)
+  sleep(1)
+  print("LED off")
+  GPIO.output(24),GPIO.LOW)
+  sleep(1)
