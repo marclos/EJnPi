dfb7dfe522d8c6f5c41a5a01d81e5a3f55bb341f
Kyle McCarty <kyle.mccarty@pomona.edu>
2020-10-16 18:00:12 -0700

    Created separate LED .py files -- one with comments and the other without.

--- led/led.py
+++ led/led.py
@@ -1,16 +1,14 @@
-import RPi.GPIO as GPIO                     # Imports the Raspberry Pi GPIO library
-from time import sleep                      # Imports the sleep function from the time module
+import RPi.GPIO as GPIO
+from time import sleep
 
-GPIO.setmode(GPIO.BCM)                      # Tells Pi what naming convention to use for GPIO pins
-GPIO.setwarnings(False)                     # Ignoring warnings for now
-GPIO.setup(24,GPIO.OUT, initial=GPIO.LOW)   # Set GPIO pin 24 for output and set initial value to logic low (off)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(24,GPIO.OUT, initial=GPIO.LOW)
 
-print("Press Ctrl + C to exit")             # Outputs this info to terminal
-
-while True:                                 # A while loop, meaning have it run forever
-  print("LED on")                           # Outputs this info to terminal
-  GPIO.output(24,GPIO.HIGH)                 # Turns 'on' the GPIO pin, meaning it supplies power (3.3V) enough to turn hte LED on
-  sleep(1)                                  # Pauses the Python program for 1 second
-  print("LED off")                          # Outputs this info to terminal
-  GPIO.output(24,GPIO.LOW)                  # Turns 'off' the GPIO pin, meaning it no longer supplies power
-  sleep(1)                                  # Pauses the Python program for 1 second
+while True:
+  print("LED on")
+  GPIO.output(24,GPIO.HIGH)
+  sleep(1)
+  print("LED off")
+  GPIO.output(24,GPIO.LOW)
+  sleep(1)
